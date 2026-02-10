6ece7c7a4b8d82d3bfdb50cae4ada4c2e6fadce1
JazelleTamayo <jazellet5@gmail.com>
2026-02-10 21:08:03 +0800

    Update db.php

--- CreateRead/db.php
+++ CreateRead/db.php
@@ -1,10 +1,26 @@
 <?php
 
+/*
+ * mysqli_report(MYSQLI_REPORT_OFF);
+ * 
+ * PURPOSE: Turns OFF automatic exception throwing for MySQL errors.
+ *          Allows traditional error checking with $conn->errno.
+ * 
+ * WHY: PHP 8.1+ throws exceptions for MySQL errors by default.
+ *      Our code expects execute() to return false on errors.
+ *      Without this: duplicate email causes fatal exception.
+ *      With this: execute() returns false, our errno check works.
+ * 
+ * PUT IN: db.php (once) - applies to all files that include it.
+ */
+mysqli_report(MYSQLI_REPORT_OFF);
+
 $conn = new mysqli("localhost", "root", "", "simplecrud");
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 
 }
+
 
 ?>
