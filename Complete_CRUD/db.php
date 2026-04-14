9389d86cd54b3740b9f7e533159388f57c7f9dc5
JazelleTamayo <jazellet5@gmail.com>
2026-04-14 08:20:30 +0800

    Update db.php

--- Complete_CRUD/db.php
+++ Complete_CRUD/db.php
@@ -1,5 +1,7 @@
 <?php
+
 $conn = new mysqli("localhost", "root", "", "simplecrud");
+mysqli_report(MYSQLI_REPORT_OFF);
 
 if ($conn->connect_error) {
     die("Connection Failed: " .$conn->connect_error);
